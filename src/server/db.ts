c3b364038ff068e05dff49b9197b23c6d4fd44fe
keytrap-x86 <badniiw@gmail.com>
2025-11-25 22:53:58 +0100

    fix again bruh

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,5 +1,4 @@
-import pkg from "$src/../generated/prisma/client";
-const { PrismaClient } = pkg;
+import { PrismaClient } from "../../generated/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { MYSQL_DATABASE, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_PORT, MYSQL_USER } from "$env/static/private";
 
