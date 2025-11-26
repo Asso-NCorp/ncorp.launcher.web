79d7cb521751130fced75ec5efbfcd7f6697f3d5
keytrap-x86 <badniiw@gmail.com>
2025-11-26 14:13:52 +0100

    rollback to previous versions because of breaking changes bruh

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,14 +1,5 @@
-import { PrismaClient } from "../../generated/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { MYSQL_DATABASE, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_PORT, MYSQL_USER } from "$env/static/private";
-
-const adapter = new PrismaMariaDb({
-    host: MYSQL_HOST,
-    port: Number(MYSQL_PORT) || 3306,
-    user: MYSQL_USER,
-    password: MYSQL_PASSWORD,
-    database: MYSQL_DATABASE,
-});
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 
 const createPrismaClient = () =>
     new PrismaClient({
@@ -16,7 +7,6 @@ const createPrismaClient = () =>
         transactionOptions: {
             maxWait: import.meta.env.MODE === "development" ? 30_000 : 5000,
         },
-        adapter,
     });
 
 const globalForPrisma = globalThis as unknown as {
